e1489123c9c181a7f08b8120241479b8476d6fe2
Janchorizo <jancho@usal.es>
2020-03-06 12:10:25 +0100

    Add theme and style header.

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -4,5 +4,11 @@ import styles from './style.module.css';
 
 export default function Component(props) {
 
-    return <span></span>;
+    return(
+    	<div className={styles.header}>
+    		<div className={"container-lg " + styles.logoContainer}>
+    			<img src="/public/img/logo-text.png"/>
+    		</div>
+    	</div>
+    	);
 }
--- src/components/scaffold/component.js
+++ src/components/scaffold/component.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import theme from '../../themes/theme.css';
 import styles from './style.module.css';
 
 export default function Component({children}) {
 
     return(
-    	<div className={"container-fluid "+styles.scaffold}>
-    		<div className="container-lg">
+    	<div className={"container-fluid px-0 "+styles.scaffold}>
     			{children}
-    		</div>
     	</div>
     	);
 }
