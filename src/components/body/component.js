6b4ad2a38bce24dfd3ed8ee75a500b789a44bd3a
Janchorizo <jancho@usal.es>
2020-03-06 13:07:49 +0100

    Add code for header.

--- src/components/body/component.js
+++ src/components/body/component.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import styles from './style.module.css';
 
+import {Title, PageManager} from '..';
+
 export default function Component(props) {
 
-    return <span></span>;
+    return(
+    	<div className={"container-lg "+styles.body}>
+    		<Title />
+    		<PageManager />
+    	</div>
+    	);
 }
--- src/components/title/component.js
+++ src/components/title/component.js
@@ -4,5 +4,10 @@ import styles from './style.module.css';
 
 export default function Component(props) {
 
-    return <span></span>;
+    return(
+	    <div className="mt-3">
+	        <h1>Categorizing Uncertainty</h1>
+	        <p><span className="d-md-block">A survey on uncertainty use and categorization.</span></p>
+    	</div>
+    	);
 }
