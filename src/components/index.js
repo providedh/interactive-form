f33d017e117f86f44fc94d166056cf1aa866ceb5
Janchorizo <jancho@usal.es>
2020-03-09 10:24:37 +0100

    Rename folder export.

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import Body from './body';
 import Footer from './footer';
 import Header from './header';
-import PageManager from './page_manager';
+import NavigationManager from './navigation_manager';
 import Scaffold from './scaffold';
 import Title from './title';
 import App from './App.js';
 
-export {Body, Footer, Header, PageManager, Scaffold, Title, App};
+export {Body, Footer, Header, NavigationManager, Scaffold, Title, App};
