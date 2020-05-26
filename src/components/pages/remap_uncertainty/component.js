a08802c41faa292a4febb8880c63fdb830f8e97c
janchorizo <jancho@usal.es>
2020-05-26 12:10:49 +0200

    Change descriptive text for the taxonomy pages.

--- src/components/pages/remap_uncertainty/component.js
+++ src/components/pages/remap_uncertainty/component.js
@@ -9,18 +9,23 @@ import Taxonomy from 'app_containers/taxonomy';
 export default function Component({title, description, useCase, useCaseType, useCaseSource}) {
 	const types = ['Imprecision', 'Ignorance', 'Credibility', 'Incompleteness']
 
-    return(
-	    <div className="my-3 container-fluid">
-	    	<h3 className="mt-3">{title}</h3>
-	    	<p>{description}</p>
-	    	<div className={`row ${styles.container}`}>
-	    		<div className="col-5 bg-blue">
+  return(
+    <div className="my-3 container-fluid">
+    	<h3 className="mt-3">{title}</h3>
+    	<p>{description}</p>
+    	<div className={`row ${styles.container}`}>
+    		<div className="col-5 bg-blue">
+    			<p>
+    				Categorizing uncertainty is a difficult task that also involves describing the taxonomy.
+    				Here we provide a curated taxonomy that we designed, use it to categorize your annotations;
+    				your categories are shown in lightgrey.
+    			</p>
 					<Taxonomy freezedCategories={types} freezed={false} editable={false}/>
-	    		</div>
-	    		<div className="col-7">
+    		</div>
+    		<div className="col-7">
 					<UnremappedSource useCase={useCase} sourceName={useCaseSource} source={Sources[useCaseSource]}/>
-	    		</div>
-	    	</div>
+    		</div>
     	</div>
-    	);
+  	</div>
+ 	);
 }
